e5b988110c031f4017abc216d56010bc31cd8964
Udit Singh <141742304+Uditsingh1234@users.noreply.github.com>
2024-09-26 18:48:49 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -15,10 +15,10 @@ function About() {
     const about = portfolioData?.abouts || {};
     const { skills, lotiUrl, description1, description2 } = about;
     return (
-        <div className='blck'>
+        <div>
             <SectionTittle title="About" />
 
-            <div className="flex w-full items-center sm:flex-col mb-10 gap-3 px-8 ssm:px-0">
+            <div className="flex w-full items-center sm:flex-col mb-10 gap-3 px-8 ssm:px-0 blck">
                 <div className=' sm:w-full p-5 flex items-center justify-center ssm:p-1 mt-10 '>
                     <div className='ssm:w-full blckk gloww '><img src={lotiUrl} alt="" className='' /></div>
                 </div>
@@ -29,7 +29,7 @@ function About() {
                 </div>
             </div>
 
-            <div className='py-10'>
+            <div className='py-10 blck'>
                 <SectionTittle title="Skills" />
                 <h1 className='text-tertiary text-xl ssm:text-center my-5'>Here are few technologies releted to my skills:</h1>
                 <div className='flex flex-wrap gap-5 pt-5'>
