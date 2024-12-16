43837b1fb4d256f46afe0c8b6fea3120a52a0e75
Udit Singh <udit6706@gmail.com>
2024-12-16 19:09:15 +0530

    change vercel.json and About.jsx file

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,37 +9,32 @@ function About() {
     return (
         <div>
             <SectionTittle title="About" />
-
-<<<<<<< HEAD
             <div className="flex w-full items-center sm:flex-col mb-10 gap-3 px-8 ssm:px-0">
-=======
-            <div className="flex w-full items-center sm:flex-col mb-10 gap-3 px-8 ssm:px-0 blck">
->>>>>>> 281f5c30d1661b30fb4a359282c01ae8033f2f85
-                <div className=' sm:w-full p-5 flex items-center justify-center ssm:p-1 mt-10 ssm:mt-10 '>
-                    <div className='ssm:w-full blckk gloww '><img src={lotiUrl} alt="" className='' /></div>
-                </div>
-                <div className='flex flex-col gap-5 w-1/2 sm:w-full ssm:text-center ml-5 mt-10 ssm:ml-0'>
-                    <p className='text-text '>{description1 || " "}</p>
+                <div className="flex w-full items-center sm:flex-col mb-10 gap-3 px-8 ssm:px-0 blck">
+                    <div className=' sm:w-full p-5 flex items-center justify-center ssm:p-1 mt-10 ssm:mt-10 '>
+                        <div className='ssm:w-full blckk gloww '><img src={lotiUrl} alt="" className='' /></div>
+                    </div>
+                    <div className='flex flex-col gap-5 w-1/2 sm:w-full ssm:text-center ml-5 mt-10 ssm:ml-0'>
+                        <p className='text-text '>{description1 || " "}</p>
 
-                    <p className='text-text'>{description2 || " "}</p>
+                        <p className='text-text'>{description2 || " "}</p>
+                    </div>
+                </div>
+
+                <div className='py-10 blck'>
+                    <SectionTittle title="Skills" />
+                    <h1 className='text-tertiary text-xl ssm:text-center my-5'>Here are few technologies releted to my skills:</h1>
+                    <div className='flex flex-wrap gap-5 pt-5'>
+                        {
+                            skills.map((skills, index) => (
+                                <div className='border border-tertiary rounded-xl py-3 px-10 cursor-pointer glasss'>
+                                    <h1 className='text-tertiary '>{skills || " "}</h1>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
-
-            <div className='py-10 blck'>
-                <SectionTittle title="Skills" />
-                <h1 className='text-tertiary text-xl ssm:text-center my-5'>Here are few technologies releted to my skills:</h1>
-                <div className='flex flex-wrap gap-5 pt-5'>
-                    {
-                        skills.map((skills, index) => (
-                            <div className='border border-tertiary rounded-xl py-3 px-10 cursor-pointer glasss'>
-                                <h1 className='text-tertiary '>{skills || " "}</h1>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-        </div>
-    )
-}
+        </div> )}
 
 export default About
